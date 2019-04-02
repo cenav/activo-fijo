97b264c18d8a46f011d9b5a3fff64f21b7832330
Cesar <cnavarro1112@gmail.com>
2019-04-02 12:18:18 -0500

    la linea viene de la clase del activo fijo

--- tai_activo_fijo_kardex.sql
+++ tai_activo_fijo_kardex.sql
@@ -5,11 +5,14 @@ CREATE OR REPLACE TRIGGER pevisa.tai_activo_fijo_kardex
     DISABLE
 DECLARE
     -- Trigger que al crear un activo fijo, replica la creacion al maestro de articulos del kardex
-    art pcarticul%ROWTYPE;
+    art   pcarticul%ROWTYPE;
+    clase activo_fijo_clase%ROWTYPE;
 BEGIN
+
     art.cod_art := :new.cod_activo_fijo;
     art.descripcion := substr(:new.descripcion, 0, 100);
-    art.linea := '2049'; --TODO
+    clase := api_activo_fijo_clase.ONEROW(:new.cod_clase);
+    art.linea := clase.linea;
     art.und := 'UND';
     art.partida := '11111111';
     art.id := '1'; -- 1 = Producto Terminado
@@ -20,11 +23,10 @@ BEGIN
     art.fecha := sysdate;
     art.fecha_modi := sysdate;
     art.cod_planta := '.';
-    art.linea_cos := '204'; --TODO
+    art.linea_cos := '204';
     art.indicador := 'CL'; -- CL = Compra Local Producto Terminado
     art.cod_unx := :new.cod_activo_fijo;
 
---     api_pcarticul.INS(art);
-
+    --     api_pcarticul.INS(art);
     sys.dbms_output.PUT_LINE(rpad('Articulo:', 12) || art.cod_art);
 END;
