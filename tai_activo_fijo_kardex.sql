1c990e4f89298095dab0c347eb0b2a59dbc1397c
Cesar <cnavarro1112@gmail.com>
2019-04-02 12:06:11 -0500

    validando data inicial

--- tai_activo_fijo_kardex.sql
+++ tai_activo_fijo_kardex.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE TRIGGER pevisa.tai_activo_fijo_kardex
     FOR EACH ROW
     DISABLE
 DECLARE
+    -- Trigger que al crear un activo fijo, replica la creacion al maestro de articulos del kardex
     art pcarticul%ROWTYPE;
 BEGIN
     art.cod_art := :new.cod_activo_fijo;
@@ -11,18 +12,16 @@ BEGIN
     art.linea := '2049'; --TODO
     art.und := 'UND';
     art.partida := '11111111';
-    art.id := '1';
+    art.id := '1'; -- 1 = Producto Terminado
     art.cta := :new.cuenta_contable;
     art.refe := 'ACTIVO FIJO';
     art.cif := 0;
     art.fob := 0;
-    art.calm := 550.5952; --TODO
-    art.fecha := sysdate; -- TODO
-    art.der := 550.5952; -- TODO
-    art.fecha_modi := sysdate; -- TODO
+    art.fecha := sysdate;
+    art.fecha_modi := sysdate;
     art.cod_planta := '.';
     art.linea_cos := '204'; --TODO
-    art.indicador := 'CL'; --TODO
+    art.indicador := 'CL'; -- CL = Compra Local Producto Terminado
     art.cod_unx := :new.cod_activo_fijo;
 
 --     api_pcarticul.INS(art);
